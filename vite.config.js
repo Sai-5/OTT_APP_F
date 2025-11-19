209b3a2957abc36cf86f794ff9a9372301eb7787
Gullapalli Durga Satya Sai <saisunny5434@gmail.com>
2025-11-19 18:33:55 +0530

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), visualizer({ open: true })],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Separate big libraries into their own chunks
+          react: ["react", "react-dom"],
+          ui: ["@mui/material", "@mui/icons-material"], // example
+        },
+      },
+      chunkSizeWarningLimit: 600, // optional, raise the warning limit
+    },
+  },
+});
